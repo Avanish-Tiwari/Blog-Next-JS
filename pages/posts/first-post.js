12f5a481b0440a34b40506e8bac26930372711d6
Avanish Tiwari <avanishtiwari@outlook.in>
2025-04-18 01:45:42 +0530

    chapter 14 completed

---
+++ pages/posts/first-post.js
@@ -0,0 +1,25 @@
+import Link from 'next/link';
+import Layout from '../../components/layout';
+import Head from 'next/head';
+import Script from 'next/script';
+ 
+export default function FirstPost() {
+  return (
+    <Layout>
+        <Head>
+            <title>First Post</title>
+        </Head>
+      <h1>First Post</h1>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() => {
+          console.log('script loaded correctly, window.FB has been populated');
+        }}
+      />
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </Layout>
+  );
+}
